104a5920c6a1c1dc313f792746b9f0dcc0dd9b1b
deepanshusinha <148999708+deepanshusinha@users.noreply.github.com>
2024-02-13 22:21:12 +0530

    Add files via upload
    basic operators in javascript

---
+++ learnjava1.js
@@ -0,0 +1,22 @@
+console.log("world");
+var number = 5;
+console.log(number);
+var string = 'hi';
+console.log(string);
+var a;
+console.log(a);
+var sum = 10 + 10;
+console.log(sum);
+var sub = 10 - 12;
+var mult = 10 * 10
+var quo = 10 / 10;
+console.log(sub);
+console.log(mult);
+console.log(quo);
+sum++
+console.log(sum)
+mult--
+console.log(mult)
+var re = 12 % 2;
+console.log(re);
+console.log("hi")
