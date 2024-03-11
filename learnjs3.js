bbe538e5de4e9d4ea0b0a08cbba38c8f8ba44a2f
deepanshusinha <148999708+deepanshusinha@users.noreply.github.com>
2024-03-11 23:35:58 +0530

    Add files via upload
    learnt arrays and array manipulation using various functions

---
+++ learnjs3.js
@@ -0,0 +1,25 @@
+var array = ["hi", 34]
+console.log(array)
+var nestedarray = [["hi","hello"],[23,34]] //nested arrays
+console.log(nestedarray)
+var myarray = [34,23,56,24]
+console.log(myarray[2])  //indexing of arrays
+console.log(myarray[2])
+myarray[0] = 22
+console.log(myarray[0]) //modify array data
+var multidimensionalarray = [[1,2,3],[3,4,5],[5,6,9]] //multidimensional array
+mydata = multidimensionalarray[1][1]
+console.log(mydata)
+var ourarray = [23,24,45]
+ourarray.push([459,35])  //push function
+console.log(ourarray)
+var array1 = [1,2,3]
+removedarray1 = array1.pop()  //pop finction
+console.log(array1)
+console.log(removedarray1)
+array2 = array1.shift()
+console.log(array2)
+array7 = array1.unshift(7)
+console.log(array7)
+
+
